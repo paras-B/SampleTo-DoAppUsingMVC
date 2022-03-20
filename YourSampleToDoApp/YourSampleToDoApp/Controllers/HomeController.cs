b6c8757251a40d7d9a6f17998cb78f65f66781f6
paras bhatia <parasbhatia999@gmail.com>
2022-03-19 22:20:34 -0400

    Add files via upload

---
+++ YourSampleToDoApp/YourSampleToDoApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using YourSampleToDoApp.Data;
+using YourSampleToDoApp.Models;
+
+namespace YourSampleToDoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public HomeController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<TaskCategory> objTaskCategoryList = _db.TaskCategories;
+            Console.WriteLine($"length of list : {objTaskCategoryList.Count()}");
+            return View(objTaskCategoryList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ YourSampleToDoApp/YourSampleToDoApp/Controllers/TaskCategoryController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourSampleToDoApp.Data;
+using YourSampleToDoApp.Models;
+
+namespace YourSampleToDoApp.Controllers
+{
+    public class TaskCategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public TaskCategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        
+        // Get Method
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+
+        //Get Method
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        // Post Method
+         [HttpPost]
+         [ValidateAntiForgeryToken]
+        public IActionResult Edit(TaskCategory obj)
+        {
+            if (obj.TaskName == obj.TaskStatus)
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.TaskCategories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(obj);
+        }
+        
+        // Post Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateTask(TaskCategory obj)
+        {
+            if (obj.TaskName == obj.TaskStatus)
+            {
+                ModelState.AddModelError("Task Name ", "The task status cannot exactly match the task name .");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.TaskCategories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(obj);
+        }
+    }
+}
