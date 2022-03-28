fa53ee3e19d906686118a1ed1abf01ba78ae1b6c
paras bhatia <parasbhatia999@gmail.com>
2022-03-27 21:30:09 -0400

    Add files via upload

--- YourSampleToDoApp/YourSampleToDoApp/Controllers/TaskCategoryController.cs
+++ YourSampleToDoApp/YourSampleToDoApp/Controllers/TaskCategoryController.cs
@@ -42,6 +42,7 @@ namespace YourSampleToDoApp.Controllers
             {
                 _db.TaskCategories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Home");
             }
             return View(obj);
@@ -60,9 +61,35 @@ namespace YourSampleToDoApp.Controllers
             {
                 _db.TaskCategories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Home");
             }
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var TaskCategoryFromDb = _db.TaskCategories.Find(id);
+            if(TaskCategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(TaskCategoryFromDb);
+        }
+        
+        //Post Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(TaskCategory obj)
+        {
+            _db.TaskCategories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Home");
         }
     }
 }
